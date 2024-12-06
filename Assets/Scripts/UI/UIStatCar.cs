cd7c9188e0bd83407ec5ee327d69a9e1a6c39aa7
DATLT <datlt@sonat.vn>
2024-12-06 11:45:17 +0700

    [Fix] stat car

--- Assets/Scripts/UI/UIStatCar.cs
+++ Assets/Scripts/UI/UIStatCar.cs
@@ -8,17 +8,17 @@ using UnityEngine.UI;
 public class UIStatCar : MonoBehaviour
 {
     [SerializeField] ParkingPlotController parkingPlotController;
-    [SerializeField] ToggleGroup toggleGroup;
+    [SerializeField] List<Toggle> toggles;
     public void UpdateInfo()
     {
-        //var plotData = parkingPlotController.ParkingPlotData;
-        //foreach (var toggle in toggleGroup.GetComponentsInChildren<Toggle>())
-        //{
-        //    string strColor = toggle.name;
-        //    var uiText = toggle.GetComponent<TextMeshProUGUI>();
+        var plotData = (ParkingPlotData) parkingPlotController.Data;
+        foreach (var toggle in toggles)
+        {
+            string strColor = toggle.name;
+            var uiText = toggle.GetComponent<TextMeshProUGUI>();
 
-        //    var color = (CarColor)Enum.Parse(typeof(CarColor), strColor);
-        //    uiText.text = plotData.GetNumberByColor(color).ToString();
-        //}
+            var color = (CarColor)Enum.Parse(typeof(CarColor), strColor);
+            uiText.text = plotData.GetNumberByColor(color).ToString();
+        }
     }
 }
